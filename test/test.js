5f2ff465053624378c5060420f2645117a08a3e1
Jacky <flashlib@gmail.com>
2017-05-13 06:32:11 +0800

    1 mutation--refactor, green

--- test/test.js
+++ test/test.js
@@ -15,6 +15,7 @@ describe('Minimum Genetic Mutation', function() {
 	describe('1 mutation', function() {
 		it('1 mutation', function() {
 			assert(_.minMutation('AACCGGTT', 'AACCGGTA', ['AACCGGTA']) == 1);
+			assert(_.minMutation('AACCGGTT', 'AACCGATT', ['AACCGATT']) == 1);
 		});
 	});
 });

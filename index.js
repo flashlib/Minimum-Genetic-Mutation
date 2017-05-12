f53b64164e856d17dda32013bbd4882261da0aec
Jacky <flashlib@gmail.com>
2017-05-13 06:31:26 +0800

    1 mutation--refactor, green

--- index.js
+++ index.js
@@ -13,7 +13,28 @@ var minMutation = function(start, end, bank) {
 		return -1;
 	}
 
-	return 1;
+	function diffElement (a, b) {
+		var arr1 = a.split('');
+		var arr2 = b.split('');
+
+		var totalDiffElement = arr1.map(function (item, index, arr) {
+			if (arr2[index] === item) {
+				return 0;
+			}
+			return 1;
+		}).reduce(function (acc, val) {
+			return acc + val;
+		}, 0);
+
+		console.log('diffElement ' + totalDiffElement);
+		return totalDiffElement;
+	}
+
+	if (diffElement(start, end) === 1) {
+		return 1;
+	}
+
+	return -1;
 };
 
 module.exports = {

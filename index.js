a65d11ce8fcdb3afb797964cd5a080f44f137c52
Jacky <flashlib@gmail.com>
2017-05-13 07:05:38 +0800

    2 mutation--red, refactor

--- index.js
+++ index.js
@@ -9,20 +9,16 @@ var minMutation = function(start, end, bank) {
 		return -1;
 	}
 
-    if (bank.indexOf(end) === -1) {
-		return -1;
-	}
-
-	function diffElement (a, b) {
+	function diffElement(a, b) {
 		var arr1 = a.split('');
 		var arr2 = b.split('');
 
-		var diffElements = arr1.map(function (item, index, arr) {
+		var diffElements = arr1.map(function(item, index, arr) {
 			if (arr2[index] === item) {
 				return -1;
 			}
 			return index;
-		}).filter(function (item, index) {
+		}).filter(function(item, index) {
 			return item !== -1;
 		});
 
@@ -30,11 +26,24 @@ var minMutation = function(start, end, bank) {
 		return diffElements;
 	}
 
-	if (diffElement(start, end).length === 1) {
-		return 1;
-	}
+	// var mutations = [];
+	// bank.forEach( function(element, index) {
+	// 	if (diffElements(start, element).length === 1) {
+	// 		mutations.push(minMutation(element, end, bank.filter(function (element, i) {
+	// 			return i !== index;
+	// 		})) + 1);
+	// 	}
+	// });
+	function searchMutation(start, end, bank) {
+		if (bank.indexOf(end) === -1) {
+			return -1;
+		}
 
-	return -1;
+		if (diffElement(start, end).length === 1) {
+			return 1;
+		}
+	}
+	return searchMutation(start, end, bank);
 };
 
 module.exports = {
